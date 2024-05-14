8ea9d0fbb402f7ad8a48f8faf1419ad7fa2cb0a7
Ruzait Ahamed <111993066+ruzait@users.noreply.github.com>
2024-05-14 21:13:17 +0530

    Write first function

---
+++ JavaScript/Passenger counter app/07_Write_first_function/script.js
@@ -0,0 +1,7 @@
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+function printRusaid() {
+    console.log(42)
+    /* console.log("Hello! Rusaid") */
+}
