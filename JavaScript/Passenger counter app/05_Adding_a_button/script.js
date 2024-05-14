f09c4e8dda0e9c3814ef185744be9e33c3648c2e
Ruzait Ahamed <111993066+ruzait@users.noreply.github.com>
2024-05-14 21:11:13 +0530

    Adding button

---
+++ JavaScript/Passenger counter app/05_Adding_a_button/script.js
@@ -0,0 +1,29 @@
+// document.getElementById('count-el').innerText = 5
+
+let count = 5 
+/* 
+count = 3
+
+count = 1
+ */
+
+// count = count + 1
+console.log(count)
+
+
+/* // assingment
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 25
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 70
+console.log(bonusPoints) */
