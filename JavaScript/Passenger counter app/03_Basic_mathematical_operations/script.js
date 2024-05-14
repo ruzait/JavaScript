b24f3fa84433f9221c93a2d57751aef8c3659b93
Ruzait Ahamed <111993066+ruzait@users.noreply.github.com>
2024-05-14 21:08:19 +0530

    Basic mathematical operations

---
+++ JavaScript/Passenger counter app/03_Basic_mathematical_operations/script.js
@@ -0,0 +1,33 @@
+// document.getElementById('count-el').innerText = 5
+
+// Basic Maths operators
+
+/* let count = 5 + 7 */
+/* let count = 5 - 7 */
+/* let count = 5 * 7 */
+/* let count = 5 / 7 */
+
+// variable asigment
+/* 
+let firstBatch = 5
+let secondBatch = 7
+
+let count = firstBatch + secondBatch
+ */
+
+// console.log(count)
+
+// Assigment
+
+// 1. Create two variables, myAge and humanDogRatio (1 human year = 7 dog years)
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+
+let myAge = 20
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
