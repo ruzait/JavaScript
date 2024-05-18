fcdbee36681a2d80992161c73c06855bf502ed88
Ruzait Ahamed <111993066+ruzait@users.noreply.github.com>
2024-05-18 19:41:07 +0530

    Add files via upload

---
+++ JavaScript/Passenger counter app/19_Debugging_online/script.js
@@ -0,0 +1,17 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    /* saveEl.innerText += countStr */
+    saveEl.textContent += countStr
+}
+
+// Google:
+// innerText alternative mdn
